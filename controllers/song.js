57adba12ffbf1adaf5e7bc7fe6ea7cd0b3de5b03
isaias <isaias.gaona@zentro.com.mx>
2025-04-10 09:01:41 -0600

    avances

---
+++ controllers/song.js
@@ -0,0 +1,91 @@
+const Song = require('../models/song');
+
+const save = async (req, res) => {
+    const song = new Song(req.body);
+    try {
+        const songStored = await song.save();
+        return res.status(200).send({ song: songStored });
+    } catch (error) {
+        return res.status(500).send({ message: 'Error al guardar la canción' });
+    }
+}
+
+const upload = async (req,res) =>{
+
+  if(!req.file) {
+    return res.status(400).send({
+      status: "error",
+      message: "No se han subido archivos",
+    });
+  }
+
+  let image = req.file.originalname;
+
+  const imageSplit = image.split("\.");
+  const extension = imageSplit[1];
+
+  // Comprobar la extensión
+  const valid_extensions = ["png", "jpg", "jpeg", "gif"];
+  if (!valid_extensions.includes(extension)) {
+    const filePath = req.file.path;
+    const fileDeleted = fs.unlinkSync(filePath);
+
+    return res.status(400).send({
+      status: "error",
+      message: "La extensión no es válida",
+    });
+  }
+
+  //Si es correcta, guardar imagen en la base de datos
+  try {
+    const songUpdated = await Song.findByIdAndUpdate(
+      {_id:req.user.id},
+      { image: req.file.filename },
+      { new: true }
+    );
+
+    if (!songUpdated) {
+      return res.status(500).send({
+        status: "error",
+        message: "Error al guardar la imagen decla canción",
+      });
+    }
+
+    return res.status(200).send({
+      status: "success",
+      song: songUpdated,
+      file: req.file,
+    });
+  } catch (err) {
+    return res.status(500).send({
+      status: "error",
+      message: "Error al guardar la imagen de la canción",
+      error: err,
+    });
+  }
+}
+
+const image = async (req, res) => {
+  // Recoger el parámetro de la url
+  const file = req.params.file;
+
+  // Comprobar si existe el fichero
+  const filePath = "./uploads/songs/" + file;
+  fs.stat(filePath, (err, exists) => {
+    if (err || !exists) {
+      return res.status(404).send({
+        status: "error",
+        message: "No existe la imagen",
+      });
+    }
+
+    // Devolver el archivo
+    return res.sendFile(path.resolve(filePath));
+  });
+}
+
+module.exports = {
+    save,
+    upload,
+    image,
+}
--- routes/song.js
+++ routes/song.js
@@ -7,8 +7,26 @@ const router = express.Router();
 // Importar controlador
 const SongController = require('../controllers/song');
 
+// Importar middleware de autenticación
+const check = require('../middlewares/auth'); // Middleware de autenticación
+
+// Configuración de subida
+const multer = require('multer')
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './uploads/songs/')
+    },
+    filename: (req, file, cb) => {
+        cb(null, 'songs-' + Date.now() + '-' + file.originalname)
+    }
+})
+const uploads = multer({ storage: storage }).single('file0')
+
 // Definir rutas
 router.get('/prueba', SongController.prueba)
+router.post('/save', check.auth, SongController.save)
+router.post('/upload', [check.auth, uploads.single('file0')], SongController.upload)
+router.get('/image/:file', SongController.image)
 
 module.exports = {
     router
